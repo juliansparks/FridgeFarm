68079c2dbc9cdccb8045d611d3178a7367e2c3b1
sketch <>
2018-04-10 23:57:22 +0000

    update pre-commit hook

--- scripts/pre-commit.bash
+++ scripts/pre-commit.bash
@@ -4,6 +4,7 @@ echo "Running pre-commit hook"
 flask test lint
 
 if [ $? -ne 0 ]; then
- echo "Code must pass linter (flask test lint) before commit!"
- exit 1
+    echo "======================================================"
+    echo "Code must pass linter (flask test lint) before commit!"
+    exit 1
 fi
